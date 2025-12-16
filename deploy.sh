81972a750837097304c5f46bc08728d9db6dd2c0
Hikashi <hikashimlbb@gmail.com>
2025-12-16 14:27:54 +0700

    Enhanced deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,27 @@
 
 echo ""
 
-# Checking for variable
-if [[ -z "$DEPLOY_ENV" ]]; then
-    echo -n "Is application in production mode (Y/n): "
-    read DEPLOY_ENV
-    
-    if [[ $DEPLOY_ENV != "n" ]]; then
-        DEPLOY_ENV=prod
-        echo "Deploying for Production mode..."
-    else
-        DEPLOY_ENV=dev
-        echo "Deploying for Development mode..."
-    fi
-fi
+PS3="Your option (1-2): "
+echo "Select the Deploying Environment:"
+select DEPLOY_ENV in Production Development; do
+    case "$REPLY" in
+        1)
+            DEPLOY_ENV=prod
+            break
+            ;;
+        2)
+            DEPLOY_ENV=dev
+            break
+            ;;
+        "custom")
+            read DEPLOY_ENV
+            break
+            ;;
+        *)
+            echo "Invalid option. Please, try again."
+            ;;
+    esac
+done
 
 # Add Ingress Nginx
 echo Applying Ingress Nginx Controller...
@@ -26,7 +34,22 @@ kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisione
 
 # Wait for Ingress Nginx is Ready
 echo Waiting for Ingress Nginx Controller is available...
-kubectl wait --for="condition=Ready" pod --selector="app.kubernetes.io/name=ingress-nginx" -n ingress-nginx --timeout=60s >/dev/null
+for (( ATTEMPT=1; ATTEMPT<=10; ATTEMPT+=1 )); do
+    STATUS=$(kubectl get pod -A -l 'app.kubernetes.io/name=ingress-nginx' -o jsonpath='{.items[*].status.conditions[?(@.type=="Ready")].status }')
+
+    if [[ $STATUS == "True" ]]; then
+        echo Attempt $ATTEMPT: Ingress Nginx Controller is Available!
+        break
+    fi
+
+    echo Attempt $ATTEMPT: Ingress Ngnix still is not available...
+    sleep 5
+done
+
+if [[ $STATUS != "True" ]]; then
+    echo Ingress Nginx Running is Failed. Please, try again.
+    exit 1
+fi
 
 # Add Custom Manifests
 echo Applying Custom Manifests...
